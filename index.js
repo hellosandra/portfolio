e011cdcf09c47326a4472db1f52dfedcd44b782c
hellosandra <ntsoeleoff@gmail.com>
2018-10-15 01:06:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Express
+const express           = require("express");
+const app               = express();
+
+// Dependencies
+const expHBS            = require("express-handlebars").create({defaultLayout: "main"});
+
+// Custom modules
+const config            = require("./config/config");
+const routes            = require("./routes/main-routes");
+
+
+// Setup view engine
+app.engine("hbs", expHBS.engine);
+app.set("view engine", "hbs")
+
+// Middleware
+app.use(express.static(__dirname + "/public/"))
+app.use("/", routes);
+
+// Custom Error Pages
+// 404
+app.use( (req, res) => {
+    res.render("404");
+})
+
+// 500
+app.use( (req, res, next) => {
+    res.status(500).render("500");
+})
+
+app.listen(config.PORT, () => {
+    console.log("Server started on port: ", config.PORT);
+})
